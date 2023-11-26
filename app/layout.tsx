aca42003fc050dd4a18eb29485e7f056b6f4b19b
sofiabaezzato <sofiabaezzato331@gmail.com>
2023-11-26 21:58:15 +0100

    project setup

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Sofia | Developer',
+  description: 'Sofia is a developer with no experience (yet)',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-slate-50`}>{children}</body>
+    </html>
+  )
+}
